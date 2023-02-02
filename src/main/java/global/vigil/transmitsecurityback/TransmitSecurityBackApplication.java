7238740aed1aa971e8fb7f09e981f02d531f274a
Felipe Alexandre <felipeomachado@gmail.com>
2023-02-02 19:03:05 -0300

    project created

---
+++ src/main/java/global/vigil/transmitsecurityback/TransmitSecurityBackApplication.java
@@ -0,0 +1,13 @@
+package global.vigil.transmitsecurityback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransmitSecurityBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransmitSecurityBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/global/vigil/transmitsecurityback/TransmitSecurityBackApplicationTests.java
@@ -0,0 +1,13 @@
+package global.vigil.transmitsecurityback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransmitSecurityBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
