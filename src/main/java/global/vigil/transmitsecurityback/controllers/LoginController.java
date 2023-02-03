ea347b953336b4d3ad95e771d604f854b6e25d77
Felipe Alexandre <felipeomachado@gmail.com>
2023-02-02 21:02:54 -0300

    Swagger configured

---
+++ src/main/java/global/vigil/transmitsecurityback/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package global.vigil.transmitsecurityback.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    String test() {
+        return "ok";
+    }
+}
